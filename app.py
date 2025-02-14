e225faea75f6378192d3963dad7ea2109d58f77e
Tom David Mueller <tom.mueller@beachouse.de>
2025-02-14 11:33:34 +0200

    remove template files

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
 import streamlit as st
 from pathlib import Path
-# For some reason the windows version only works if this is imported here
-import pyopenms
 
 if __name__ == '__main__':
     pages = {
         "PeakStorm" : [
             st.Page(Path("content", "quickstart.py"), title="Quickstart", icon="👋"),
-            st.Page(Path("content", "peakstorm.py"), title="Isotopic Envelope Calculator", icon="📖"),
+            st.Page(Path("content", "peakstorm.py"), title="Isotopic Envelope Calculator", icon="�"),
         ]
     }
 
