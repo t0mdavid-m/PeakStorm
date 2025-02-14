e74241f81f0f6f9ddb0d518315e89d598b4a68b7
Tom David Mueller <tom.mueller@beachouse.de>
2025-02-14 11:40:05 +0200

    update quickstart

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ if __name__ == '__main__':
     pages = {
         "PeakStorm" : [
             st.Page(Path("content", "quickstart.py"), title="Quickstart", icon="👋"),
-            st.Page(Path("content", "peakstorm.py"), title="Isotopic Envelope Calculator", icon="�"),
+            st.Page(Path("content", "peakstorm.py"), title="Isotopic Envelope Calculator", icon="📶"),
         ]
     }
 
